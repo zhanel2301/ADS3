5634047adad2756f74c39b43916efb6ed5e54b17
User <231608@astanait.edu.kz>
2024-05-10 08:39:45 +0500

    Initial commit

---
+++ src/HeapSort.java
@@ -0,0 +1,41 @@
+public class HeapSort {
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 55, 8, 2, 3, 100};
+        heapSort(arr);
+        System.out.println("Sorted (heap sort): " + java.util.Arrays.toString(arr));
+    }
+}
